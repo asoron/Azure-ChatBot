ae0c2838d60333c30de47436cd8cb64c8b7b3b19
Asrin Yilmaz <34812088+asoron@users.noreply.github.com>
2024-08-19 09:54:29 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB6XnCBW79QbDK2FEyGCwmiexNrlPWYFzo",
-	authDomain: "azurechat-91eae.firebaseapp.com",
-	projectId: "azurechat-91eae",
-	storageBucket: "azurechat-91eae.appspot.com",
-	messagingSenderId: "1034329999347",
-	appId: "1:472477644017:web:d81641027442bbb10fa550",
-	appId: "1:1034329999347:web:f06ec770bd10ebaef37486",
-  	measurementId: "G-FR5B8T5DG8"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
